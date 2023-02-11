425b9cd3220d5be4e78096264a3f2c5f90188b4b
0x4E43 <talk2nimai@gmail.com>
2023-02-12 00:43:16 +0530

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { A } from "solid-start";
+import Login from "~/components/auth/Login";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <Login/>
+    </main>
+  );
+}
